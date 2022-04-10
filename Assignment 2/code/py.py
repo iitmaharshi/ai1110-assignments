1810efdcbd0a1522e787f1fab4d5a8710d76ebc3
iitmaharshi <102573460+iitmaharshi@users.noreply.github.com>
2022-04-10 12:04:52 +0530

    Add files via upload

--- Assignment 2/code/py.py
+++ Assignment 2/code/py.py
@@ -1,1 +1,17 @@
+import mpmath as mp
+import numpy as np
 
+
+a = float(input())
+b = float(input())
+
+c = float(mp.tan(mp.pi - mp.atan(a) - mp.atan(b)))
+
+x = a + b + c
+y = a*b*c
+epsilon = 10**-4
+
+if abs(x-y) < epsilon:
+    print("verified and true")
+else:
+    print("false")
