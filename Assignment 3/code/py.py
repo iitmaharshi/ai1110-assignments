794f811e0e625c351d22605b46458254f267a13b
iitmaharshi <102573460+iitmaharshi@users.noreply.github.com>
2022-04-26 14:35:48 +0530

    Add files via upload

--- Assignment 3/code/py.py
+++ Assignment 3/code/py.py
@@ -1,31 +1,26 @@
-import numpy as np
-import pandas as pd
 import openpyxl
+from collections import Counter
+import pandas as pd
 from openpyxl.styles import Alignment
-read = pd.read_excel("raw_data.xlsx","Sheet1")
-data = np.array(read)
-values = list(data[0])
-a = values.count(0)
-b = values.count(1)
-c = values.count(2)
-d = values.count(3)
-e = values.count(4)
-f = values.count(5)
-g = values.count(6)
-h = values.count(7)
-i = values.count(8)
-j = values.count(9)
 
-digits = [0,1,2,3,4,5,6,7,8,9]
-frequencies = [a,b,c,d,e,f,g,h,i,j]
-maximum = max(a,b,c,d,e,f,g,h,i,j)
-minimum =  min(a,b,c,d,e,f,g,h,i,j)
-dictionary = {a:0,b:1,c:2,d:3,e:4,f:5,g:6,h:7,i:8,j:9}
-print(f'The Most occuring digit : {dictionary[maximum]}')
-print(f'The least occuring digit : {dictionary[minimum]}')
-write = pd.DataFrame({"Digits":digits,"Frequency\n":frequencies})
-write.to_excel("frequency.xlsx",index=False)
+df = pd.read_excel('raw_data.xlsx', 'Sheet1', header=None)
+a = df.to_numpy()
+df = pd.DataFrame(a)
 
+newlst = []
+df.applymap(lambda x: newlst.append(x))
+newlst.sort()
+
+cou = Counter(newlst)
+digits = list(cou.keys())
+freq = list(cou.values())
+lt = len(digits)
+total = len(newlst)
+
+df1 = pd.DataFrame()
+df1['Digits'] = digits
+df1['Frequency\n'] = freq
+df1.to_excel('frequency.xlsx', index=False)
 wb=openpyxl.load_workbook("frequency.xlsx")
 ws=wb['Sheet1']
 ws['A12']='Total'
