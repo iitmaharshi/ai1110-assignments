4a10500e9ad419067afc3605d9331bac5214cc10
iitmaharshi <102573460+iitmaharshi@users.noreply.github.com>
2022-07-04 12:33:44 +0530

    Add files via upload

--- manual 1/code/1.2_plot.py
+++ manual 1/code/1.2_plot.py
@@ -1,6 +1,7 @@
 #Importing numpy, scipy, mpmath and pyplot
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
 #if using termux
 import subprocess
@@ -9,27 +10,38 @@ import shlex
 
 
 
-x = np.linspace(-4,4,30)#points on the x axis
+x = np.linspace(-4,4,80)#points on the x axis
+
+
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
 randvar = np.loadtxt('uni.dat',dtype='double')
-for i in range(0,30):
+for i in range(0,80):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
 
-	
-plt.plot(x.T,err)#plotting the CDF
+def func(x):
+	if x>=0 and x<=1:
+		return x
+	elif x>1:
+		return 1
+	else: return 0
+
+vect = scipy.vectorize(func,otypes=[np.float])
+
+plt.plot(x.T,err,'o')#plotting the CDF
+plt.plot(x,vect(x))
 plt.grid() #creating the grid
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
-
+plt.legend(["Simulation", "Analysis"])
 #if using termux
 #plt.savefig('../figs/uni_cdf.pdf')
 #plt.savefig('../figs/uni_cdf.eps')
 #subprocess.run(shlex.split("termux-open ../figs/uni_cdf.pdf"))
 #if using termux
-plt.savefig('./figs/uni_cdf.pdf')
+plt.savefig('./figs/uni_cdf.png')
 #plt.savefig('../figs/gauss_cdf.eps')
 #subprocess.run(shlex.split("termux-open ../figs/gauss_cdf.pdf"))
 #else
